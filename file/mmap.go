da0b9c8ff1f4e4471740c8db357f2a195180d5f3
Zaire404 <3147879462@qq.com>
2025-02-01 16:47:01 +0800

    feat: add mmap file

---
+++ file/mmap.go
@@ -0,0 +1,69 @@
+package file
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Zaire404/ZDB/util"
+	"github.com/pkg/errors"
+)
+
+type MmapFile struct {
+	Data []byte
+	Fd   *os.File
+}
+
+func OpenMmapFile(filePath string, flag int, size int) (*MmapFile, error) {
+	if size < 0 {
+		return nil, errors.Errorf("invalid size: %d", size)
+	}
+
+	fd, err := os.OpenFile(filePath, flag, 0666)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to open file %s", filePath)
+	}
+
+	fileInfo, err := fd.Stat()
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to stat file %s", filePath)
+	}
+	fileSize := fileInfo.Size()
+	if fileSize == 0 {
+		// file is empty, truncate it
+		if err := fd.Truncate(int64(size)); err != nil {
+			return nil, errors.Wrapf(err, "failed to truncate file %s", filePath)
+		}
+		fileSize = int64(size)
+		// To be optimized
+		go fd.Sync()
+	}
+	writable := flag&os.O_RDWR == os.O_RDWR
+	buf, err := util.Mmap(fd, int(fileSize), writable)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to mmap file %s with size: %d", filePath, fileSize)
+	}
+	return &MmapFile{
+		Data: buf,
+		Fd:   fd,
+	}, nil
+}
+
+func (m *MmapFile) Sync() error {
+	if m == nil {
+		return nil
+	}
+	return util.Msync(m.Data)
+}
+
+func (m *MmapFile) Close() error {
+	if m.Fd == nil {
+		return nil
+	}
+	if err := m.Sync(); err != nil {
+		return fmt.Errorf("while sync file: %s, error: %v", m.Fd.Name(), err)
+	}
+	if err := util.Munmap(m.Data); err != nil {
+		return fmt.Errorf("while munmap file: %s, error: %v", m.Fd.Name(), err)
+	}
+	return m.Fd.Close()
+}
---
+++ util/mmap.go
@@ -0,0 +1,28 @@
+package util
+
+import (
+	"os"
+
+	"golang.org/x/sys/unix"
+)
+
+// Mmap maps a file into memory.
+func Mmap(fd *os.File, size int, writable bool) ([]byte, error) {
+	var mode int
+	if writable {
+		mode = unix.PROT_READ | unix.PROT_WRITE
+	} else {
+		mode = unix.PROT_READ
+	}
+	return unix.Mmap(int(fd.Fd()), 0, size, mode, unix.MAP_SHARED)
+}
+
+// Msync flushes changes made to the in-core copy of a file that was mapped into memory using Mmap.
+func Msync(data []byte) error {
+	return unix.Msync(data, unix.MS_SYNC)
+}
+
+// Munmap unmaps a file from memory.
+func Munmap(data []byte) error {
+	return unix.Munmap(data)
+}
