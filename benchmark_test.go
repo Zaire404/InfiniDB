f68da6ae0cda0fa7ffe010f5efdf3e2538e96476
Zaire404 <3147879462@qq.com>
2025-03-17 13:32:23 +0800

    update

--- benchmark_test.go
+++ benchmark_test.go
@@ -12,13 +12,23 @@ type BenchmarkOptions struct {
 	ValueSize int
 }
 
-var benchOpt = &BenchmarkOptions{
-	KeySize:   16,
-	ValueSize: 128,
+var benchOpts = []BenchmarkOptions{
+	{KeySize: 16, ValueSize: 64},
+	{KeySize: 16, ValueSize: 128},
+	{KeySize: 16, ValueSize: 256},
+	{KeySize: 16, ValueSize: 512},
+	{KeySize: 16, ValueSize: 1024},
+	{KeySize: 16, ValueSize: 2048},
+	{KeySize: 16, ValueSize: 4096},
+	{KeySize: 16, ValueSize: 8192},
 }
 
 func BenchmarkFillRandom(b *testing.B) {
-	FillRandom(b, benchOpt)
+	for _, opt := range benchOpts {
+		b.Run(fmt.Sprintf("KeySize=%dbytes,ValueSize=%dbytes ", opt.KeySize, opt.ValueSize), func(b *testing.B) {
+			FillRandom(b, &opt)
+		})
+	}
 }
 
 func FillRandom(b *testing.B, benchOpt *BenchmarkOptions) {
@@ -40,10 +50,15 @@ func FillRandom(b *testing.B, benchOpt *BenchmarkOptions) {
 			b.Logf("Set error for key%d: %v", i, err)
 		}
 	}
+	b.StopTimer()
 }
 
 func BenchmarkFillSeq(b *testing.B) {
-	FillSeq(b, benchOpt)
+	for _, opt := range benchOpts {
+		b.Run(fmt.Sprintf("KeySize=%dbytes,ValueSize=%dbytes ", opt.KeySize, opt.ValueSize), func(b *testing.B) {
+			FillSeq(b, &opt)
+		})
+	}
 }
 
 func FillSeq(b *testing.B, benchOpt *BenchmarkOptions) {
@@ -66,10 +81,15 @@ func FillSeq(b *testing.B, benchOpt *BenchmarkOptions) {
 			b.Logf("Set error for key%d: %v", i, err)
 		}
 	}
+	b.StopTimer()
 }
 
 func BenchmarkReadSeq(b *testing.B) {
-	ReadSeq(b, benchOpt)
+	for _, opt := range benchOpts {
+		b.Run(fmt.Sprintf("KeySize=%dbytes,ValueSize=%dbytes ", opt.KeySize, opt.ValueSize), func(b *testing.B) {
+			ReadSeq(b, &opt)
+		})
+	}
 }
 
 func ReadSeq(b *testing.B, benchOpt *BenchmarkOptions) {
@@ -99,9 +119,14 @@ func ReadSeq(b *testing.B, benchOpt *BenchmarkOptions) {
 			b.Logf("Get error for key%d: %v", i, err)
 		}
 	}
+	b.StopTimer()
 }
 func BenchmarkReadRandom(b *testing.B) {
-	ReadRandom(b, benchOpt)
+	for _, opt := range benchOpts {
+		b.Run(fmt.Sprintf("KeySize=%dbytes,ValueSize=%dbytes ", opt.KeySize, opt.ValueSize), func(b *testing.B) {
+			ReadRandom(b, &opt)
+		})
+	}
 }
 
 func ReadRandom(b *testing.B, benchOpt *BenchmarkOptions) {
@@ -132,4 +157,5 @@ func ReadRandom(b *testing.B, benchOpt *BenchmarkOptions) {
 			b.Logf("Get error for key%d: %v", i, err)
 		}
 	}
+	b.StopTimer()
 }
